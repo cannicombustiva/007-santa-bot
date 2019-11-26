2ee449a6de20dc678bd242e107738339fe7938e1
Salvatore Difranco <salvatoredifranco8@gmail.com>
2019-11-26 16:23:26 +0100

    try to store files using fs

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
-const Telegraf = require('telegraf')
+const TelegramBot = require('node-telegram-bot-api');
+const token = "999735973:AAFzDNHXDunkFsV78Kr0aPA4F0YwPkjGQ7w";
 
-const bot = new Telegraf("999735973:AAFzDNHXDunkFsV78Kr0aPA4F0YwPkjGQ7w")
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+// I try to create a file system to store the IDs 
+var file = require('file-system');
+var fs = require('fs');
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  console.log(msg.from.id)
+  const chatId = msg.chat.id;
+
+  fs.appendFile('storing.json', 'diofa!1', function(err) {
+    if(err) throw err;
+    console.log("saved!")
+  })
+  
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+const storeCheck = (chatId) => {
+  fs.readFile('storing.json', function (err, data) {
+    var json = JSON.parse(data)
+    json.push('search result: ' + currentSearchResult)
+
+    fs.writeFile("results.json", JSON.stringify(json))
+})
+
+}
+
+//999735973:AAFzDNHXDunkFsV78Kr0aPA4F0YwPkjGQ7w
+//176835657
