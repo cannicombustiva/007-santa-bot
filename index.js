3eacff38ce0f895ea38d168347887f43cf3ff316
Salvatore Difranco <salvatore.difranc@di.unito.it>
2019-12-19 13:11:11 +0100

    updated index and mixer

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const fs = require('fs-extra');
 const TelegramBot = require('node-telegram-bot-api');
+
+const fs = require('fs-extra');
+
 const mixer = require('./mixer');
 
-const token = "YOUR_TOKEN_HERE";
+const token = "999735973:AAFzDNHXDunkFsV78Kr0aPA4F0YwPkjGQ7w";
 const storeFileName = './storing.json';
 
 const initialStore = {
   users: {},
   pairedUsers: []
 };
-
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
@@ -22,8 +23,8 @@ const getStore = () => fs.readJson(storeFileName);
 
 
 const addUser = async (id, user) => {
-  const userExist = Boolean(store.users[id]);
   const store = await getStore();
+  const userExist = Boolean(store.users[id]);
   store.users[id] = user;
   await writeStore(store);
   return userExist;
@@ -37,18 +38,35 @@ const mixUserPair = async () => {
   await writeStore(store);
 }
 
+const checkInGroup = (msg) => {
+  if (msg.chat.type !== 'private') {
+    bot.sendMessage(msg.chat.id, 'Start a private conversation with @SadSantaBot');
+    return true;
+  } 
+  return false;
+}
+
 const start = (msg) => {
+  if (checkInGroup(msg)) {
+    return;
+  }
+
   bot.sendMessage(msg.chat.id, 'Welcome', {
   reply_markup: {
       keyboard: [['/Join']]
   }});   
 };
 
-
 const join = async (msg) => {
+    if (checkInGroup(msg)) {
+    return;
+  }
+  
   // Destructuring msg from telegram command
   const { chat, from } = msg;
   const userExist = await addUser(from.id, { username: from.username });
+  
+  await mixUserPair();
   
   // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chat.id, 'Received your message');
@@ -56,8 +74,15 @@ const join = async (msg) => {
   if (userExist) {
     bot.sendMessage(from.id, 'User already exists');
   }
-  
-  await mixUserPair();
+};
+
+const play = async () => {
+  const store = await getStore();
+  store.pairedUsers.forEach((pair) => {
+    const [santaId, childId] = pair;
+    const username = store.users[childId].username; //passo [childId] perché è dinamico porcodddiooooooooooo
+    bot.sendMessage(santaId, `You are ${username}'s Santa`);
+  });
 };
 
 const main = async () => {              
@@ -69,6 +94,7 @@ const main = async () => {
   // Listen for any kind of message. There are different kinds of
   // messages.
   bot.onText(/\/Join/, join);
+  bot.onText(/magicplay/, play)
 };
 
 main()
